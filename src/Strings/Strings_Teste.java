4f330597c02021eae6017011ae9bbebefdb9b17d
JoaoNascimento1802 <joao.emanuel.pn@gmail.com>
2026-02-06 18:38:49 -0300

    :recycle: refactor: AtTestando lógica de strings

---
+++ src/Strings/Strings_Teste.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class Strings_Teste {
+
+    public static void main(String[]args){
+
+        String filme;
+
+        filme = """
+                o filme hobbit é um filme legal
+                do tolkien sobre o hobbit bilbo bolseiro
+                """;
+
+        System.out.println(filme);
+    }
+}
